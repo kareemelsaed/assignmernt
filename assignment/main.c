6c0ca5039cfb6192e3f14c5ad5d87792217f814d
kareem elsaed <kareemelsaed26@gmail.com>
2021-04-13 02:31:28 +0200

    this is our assignment

---
+++ assignment/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("welcome to our system\n");
+    struct student {
+    char name[25];
+    int id;
+    int date_of_birth[3];
+    int score_of_last_year;
+    };
+    return 0;
+}
